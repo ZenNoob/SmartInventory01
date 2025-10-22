5cf9c8a4da9eb70e6605e70a33f615ff6594d6a6
Nguyen Minh Phuc <phucteacher2019@gmail.com>
2025-10-22 03:38:55 +0000

    tiếp tục sửa cho tôi phần layout của giao diện. hiện tại layou chính tôi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,12 +39,10 @@ export default function RootLayout({
             <SidebarProvider>
               <MainNav />
               <SidebarInset>
-                <div className="flex flex-col sm:gap-4 sm:py-4 h-full w-[90%] mx-auto">
-                  <Header />
-                  <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-                    {children}
-                  </main>
-                </div>
+                <Header />
+                <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+                  {children}
+                </main>
               </SidebarInset>
             </SidebarProvider>
           </GlobalError>
