06c7b35c105f9e5b420a07d224bd86e0f71d9325
Nguyen Minh Phuc <phucteacher2019@gmail.com>
2025-10-22 02:32:45 +0000

    tôi muốn div này fix 90% container chứa nó

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
             <SidebarProvider>
               <MainNav />
               <SidebarInset>
-                <div className="flex flex-col sm:gap-4 sm:py-4 h-full">
+                <div className="flex flex-col sm:gap-4 sm:py-4 h-full w-[90%] mx-auto">
                   <Header />
                   <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
                     {children}
