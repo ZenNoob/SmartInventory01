0db70add1ff191f952a80ce5573a73bd99150611
Nguyen Minh Phuc <phucteacher2019@gmail.com>
2025-10-22 03:40:06 +0000

    ở giữa menu và phần main có một khoảng trống rất lớn tôi muốn khoảng trố

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
               <MainNav />
               <SidebarInset>
                 <Header />
-                <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+                <main className="flex-1 p-4 md:gap-8">
                   {children}
                 </main>
               </SidebarInset>
