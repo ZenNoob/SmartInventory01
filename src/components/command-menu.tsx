253bbe10a1cc1e746f16f7f215edbc4b90a33451
Nguyen Minh Phuc <phucteacher2019@gmail.com>
2025-10-29 15:50:25 +0000

    icon kính lúp bị trùng với test. và không cho nhập search. tôi muốn, tôi

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -81,8 +81,8 @@ export function CommandMenu() {
         )}
         onClick={() => setOpen(true)}
       >
-        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-        <span className="hidden lg:inline-flex">Tìm kiếm...</span>
+        <Search className="h-4 w-4 mr-2" />
+        <span className="hidden lg:inline-flex">Tìm kiếm & điều hướng...</span>
         <span className="inline-flex lg:hidden">Tìm kiếm...</span>
         <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
           <span className="text-xs">⌘</span>K
